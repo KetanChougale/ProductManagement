86e301b6b14168f370ed1cb1a33f94e916cfb5e3
KetanChougale <ketanchougale0909@gmail.com>
2026-07-26 20:41:38 +0530

    product.cs new add

--- ProductManagement.Domain/Entities/Product.cs
+++ ProductManagement.Domain/Entities/Product.cs
@@ -1,10 +1,12 @@
-﻿namespace ProductManagement.Domain.Entities;
+namespace ProductManagement.Domain.Entities;
 
 public class Product
 {
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
 
     public decimal Price { get; set; }
 
