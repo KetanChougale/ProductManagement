0777948bc739873d780120a8f7c9a55fd7dd1e62
KetanChougale <ketanchougale0909@gmail.com>
2026-07-26 20:28:49 +0530

    product.cs chnages adding one field

--- ProductManagement.Domain/Entities/Product.cs.cs
+++ ProductManagement.Domain/Entities/Product.cs.cs
@@ -1,16 +1,14 @@
-﻿namespace ProductManagement.Domain.Entities;
-
-public class Product
+﻿public class Product
 {
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
 
     public decimal Price { get; set; }
 
     public int StockQuantity { get; set; }
 
     public bool IsActive { get; set; }
-
-    public DateTime CreatedAt { get; set; }
 }
